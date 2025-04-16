052b91a5c4ea4d9882b810de8417e6fe457d6fb5
PriccilaLucem <priccilalucem@gmail.com>
2025-04-15 22:29:55 -0300

    exclude database

--- src/test/java/org/example/AuthenticationStoreTests.java
+++ src/test/java/org/example/AuthenticationStoreTests.java
@@ -1,16 +1,29 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@Import(TestConfig.class)
+
+
+@SpringBootTest(classes = {TestConfig.class})
 @ActiveProfiles({"test", "ci"})
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class AuthenticationStoreTests {
+    // ...
+
     @Test
     void contextLoads() {
     }
 
 }
+
